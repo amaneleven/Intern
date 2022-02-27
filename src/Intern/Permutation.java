8a5db6799159abdc5ceb300a99539c6cd469b02c
AmanEleven <hackerr647@gmail.com>
2022-02-27 14:34:34 +0530

    updateed permutation

--- src/Intern/Permutation.java
+++ src/Intern/Permutation.java
@@ -3,18 +3,18 @@ import java.util.*;
 
 public class Permutation {
     public static void main(String[] args) {
-        int totalobjects, selectedobject, permutation, f1, f2;
+        int tos, sos, permutation, f1, f2;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Value of n and r: ");
-        totalobjects = sc.nextInt();
-        selectedobject = sc.nextInt();
-        f1 = totalobjects;
-        for (int i = totalobjects - 1; i >= 1; i--)
+        tos = sc.nextInt();
+        sos = sc.nextInt();
+        f1 = tos;
+        for (int i = tos - 1; i >= 1; i--)
         {
             f1 = f1 * i;
         }
         int number;
-        number = totalobjects - selectedobject;
+        number = tos - sos;
         f2 = number;
         for (int i = number - 1; i >= 1; i--)
         {
